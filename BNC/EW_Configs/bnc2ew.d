00cbcd36931332a0841b1e87849d6c21f8c44321
Francisco J <Fran89@users.noreply.github.com>
2018-03-18 02:26:19 -0400

    EW Configurations added

---
+++ BNC/EW_Configs/bnc2ew.d
@@ -0,0 +1,25 @@
+#
+# gsof2ew configuration file
+#
+
+ModuleId        MOD_K2EW        # module id for this import
+RingName        WAVE_RING       # transport ring to use for input/output
+
+LogFile         0               # If 0, don't output to logfile; if 1, do
+                                # if 2, log to module log but not stderr/stdout
+
+HeartbeatInt    30              # Heartbeat interval in seconds
+
+Network         PR              # Network name for EW msg headers
+
+Debug         2                 # debug level: 0 or commented out for no debug
+                                # logging; 1 - 4 for increasing verbosity
+                                # default: no debug output
+                                
+InjectVel                       # inject GPS Velocity
+
+SampRate       1                # GPS Samplerate in Hz
+
+SubX 1                          # If you prefer the N
+SubY 1                          # If you prefer the E
+SubZ 1                          # If you prefer the U
